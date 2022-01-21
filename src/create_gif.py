8fd165929b719ccdbc93149ee8d57f077d985f94
Philipp Schuler <schulerp03@gmail.com>
2022-01-21 10:18:50 +0100

    minor adjustments

--- src/create_gif.py
+++ src/create_gif.py
@@ -4,6 +4,7 @@ import glob
 from rrg_db import DatabaseManager
 
 png_dir = "src/plots"
+gif_dir = "src/gifs"
 config = DatabaseManager().get_config()
 
 images = []
@@ -11,7 +12,11 @@ for file_name in sorted(os.listdir(png_dir)):
     if file_name.endswith(".png"):
         file_path = os.path.join(png_dir, file_name)
         images.append(imageio.imread(file_path))
-imageio.mimsave("src/gifs/animated3.gif", images, duration=0.2)
+imageio.mimsave(
+    "src/gifs/" + config["filename"] + "_" + str(len(os.listdir(gif_dir)) + 1) + ".gif",
+    images,
+    duration=config["gif_frame_time_delay"],
+)
 
 if config["remove_files_after_gif"]:
     files = glob.glob(png_dir + "/*")
